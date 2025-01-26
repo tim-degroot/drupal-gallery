05e026223d415f65016b73df2823096981eb5af0
Tim <47915253+tim-degroot@users.noreply.github.com>
2025-01-26 21:09:07 +0100

    v1 php

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -1,22 +1,85 @@
 <?php
+
 namespace Drupal\s3_gallery\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Aws\S3\S3Client;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
- * Provides route responses for the Example module.
+ * Provides route responses for the S3 Gallery module.
  */
 class GalleryController extends ControllerBase {
 
-  /**
-   * Returns a simple page.
-   *
-   * @return array
-   *   A simple renderable array.
-   */
-  public function myPage() {
-    return [
-    '#markup' => $this->t('This is the gallery page.'),    ];
+  protected $configFactory;
+  protected $s3Client;
+
+  public function __construct(ConfigFactoryInterface $config_factory, S3Client $s3_client) {
+    $this->configFactory = $config_factory;
+    $this->s3Client = $s3_client;
   }
 
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('aws.s3_client')
+    );
+  }
+
+  /**
+   * Returns a gallery page.
+   *
+   * @return array
+   *   A renderable array.
+   */
+  public function myPage() {
+    try {
+      // Initialize S3 client
+      $config = $this->configFactory->get('system.site')->get('aws_s3');
+      $s3 = new S3Client([
+        'version' => 'latest',
+        'region' => 'eu-central-1', // Change to your bucket's region
+        'credentials' => [
+          'key'    => $config['key'],
+          'secret' => $config['secret'],
+        ],
+      ]);
+
+      $bucket = 'acdweb-storage';
+      $prefix = 'photos/';
+
+      // Set the folder to be listed manually
+      $folder = 'photos/20241106 Boulderen/'; // Change this to the desired folder
+
+      // List objects in the selected folder
+      $objects = $s3->listObjectsV2([
+        'Bucket' => $bucket,
+        'Prefix' => $folder,
+      ]);
+
+      $output = '';
+      if (isset($objects['Contents'])) {
+        $output .= "<h1>Images in '{$folder}'</h1>";
+        $output .= "<div class='gallery'>";
+
+        foreach ($objects['Contents'] as $object) {
+          $url = $s3->getObjectUrl($bucket, $object['Key']);
+          $output .= "<img src='{$url}' alt='Image'>";
+        }
+
+        $output .= "</div>";
+      } else {
+        $output .= "No objects found in '{$folder}'.";
+      }
+
+      return [
+        '#markup' => $output,
+      ];
+    } catch (Exception $e) {
+      return [
+        '#markup' => "Error: " . $e->getMessage(),
+      ];
+    }
+  }
 }
