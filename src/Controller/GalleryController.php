5b9fe87c67fe9662ee325d7291fb3f045835d85e
Tim <47915253+tim-degroot@users.noreply.github.com>
2025-01-26 23:03:01 +0100

    split pages

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -13,6 +13,70 @@ use Symfony\Component\HttpFoundation\Request;
 class GalleryController extends ControllerBase {
 
   /**
+   * Returns the main gallery page.
+   *
+   * @return array
+   *   A renderable array.
+   */
+  public function mainPage() {
+    try {
+      // Retrieve AWS S3 configuration from settings.php
+      $config = Settings::get('aws_s3');
+      $s3 = new S3Client([
+        'version' => 'latest',
+        'region' => $config['region'],
+        'credentials' => [
+          'key'    => $config['key'],
+          'secret' => $config['secret'],
+        ],
+      ]);
+
+      $bucket = 'acdweb-storage';
+      $prefix = 'photos/';
+
+      // Print the current prefix
+      $output = "<h2>Main Photo Gallery</h2>";
+      $output .= "<h3>Current prefix: " . htmlspecialchars($prefix) . "</h3>";
+
+      // List objects in the specified prefix
+      $contents = $s3->listObjectsV2([
+        'Bucket' => $bucket,
+        'Prefix' => $prefix,
+        'Delimiter' => '/', // Ensure only direct children are listed
+      ]);
+
+      $output .= "<h3>The contents of your bucket are:</h3>";
+      $output .= "<ul>";
+      if (isset($contents['Contents'])) {
+        foreach ($contents['Contents'] as $content) {
+          $output .= "<li>" . htmlspecialchars($content['Key']) . "</li>";
+        }
+      }
+      $output .= "</ul>";
+
+      $output .= "<h3>The CommonPrefixes are:</h3>";
+      $output .= "<ul>";
+      if (isset($contents['CommonPrefixes'])) {
+        foreach ($contents['CommonPrefixes'] as $commonPrefix) {
+          $output .= "<li>" . htmlspecialchars($commonPrefix['Prefix']) . "</li>";
+        }
+      }
+      $output .= "</ul>";
+
+      // Return the output as a renderable array
+      return [
+        '#markup' => $output,
+      ];
+    } catch (\Exception $e) {
+      // Debugging information
+      \Drupal::logger('s3_gallery')->error('Error: @error', ['@error' => $e->getMessage()]);
+      return [
+        '#markup' => "Error: " . $e->getMessage(),
+      ];
+    }
+  }
+
+  /**
    * Returns a gallery page.
    *
    * @param string|null $prefix
@@ -38,27 +102,33 @@ class GalleryController extends ControllerBase {
       $prefix = 'photos/' . urldecode($prefix); // Ensure 'photos/' is prefixed and decode the prefix
 
       // Print the current prefix
-      $output = "Current prefix: " . htmlspecialchars($prefix) . "<br>";
+      $output = "<h2>Photo Gallery</h2>";
+      $output .= "<h3>Current prefix: " . htmlspecialchars($prefix) . "</h3>";
 
       // List objects in the specified prefix
       $contents = $s3->listObjectsV2([
         'Bucket' => $bucket,
         'Prefix' => $prefix,
+        'Delimiter' => '/', // Ensure only direct children are listed
       ]);
 
-      $output .= $contents;
-
-      $output .= "The contents of your bucket are: <br>";
-      foreach ($contents['Contents'] as $content) {
-        $output .= $content['Key'] . "<br>";
+      $output .= "<h3>The contents of your bucket are:</h3>";
+      $output .= "<ul>";
+      if (isset($contents['Contents'])) {
+        foreach ($contents['Contents'] as $content) {
+          $output .= "<li>" . htmlspecialchars($content['Key']) . "</li>";
+        }
       }
+      $output .= "</ul>";
 
-      
-
-      $output .= "The CommonPrefixes are: <br>";
-      foreach ($contents['CommonPrefixes'] as $commonPrefix) {
-        $output .= $commonPrefix . "<br>";
+      $output .= "<h3>The CommonPrefixes are:</h3>";
+      $output .= "<ul>";
+      if (isset($contents['CommonPrefixes'])) {
+        foreach ($contents['CommonPrefixes'] as $commonPrefix) {
+          $output .= "<li>" . htmlspecialchars($commonPrefix['Prefix']) . "</li>";
+        }
       }
+      $output .= "</ul>";
 
       // Return the output as a renderable array
       return [
