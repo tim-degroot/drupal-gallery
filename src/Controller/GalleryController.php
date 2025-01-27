9bdd2c4baaaeeb83287038407d6cf557037946d9
Tim <47915253+tim-degroot@users.noreply.github.com>
2025-01-27 01:18:46 +0100

    csss

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -162,53 +162,12 @@ class GalleryController extends ControllerBase {
               }
             }
       $output .= "</div>";
-
-      $output .= "<style>
-.grid-wrapper > div > img {
-    max-width: 100%;
-    height: auto;
-    vertical-align: middle;
-    display: inline-block;
-}
-
-/* Main CSS */
-.grid-wrapper > div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-.grid-wrapper > div > img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 5px;
-}
-
-.grid-wrapper {
-    display: grid;
-    grid-gap: 10px;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    grid-auto-rows: 200px;
-    grid-auto-flow: dense;
-}
-.grid-wrapper .wide {
-    grid-column: span 2;
-}
-.grid-wrapper .tall {
-    grid-row: span 2;
-}
-.grid-wrapper .big {
-    grid-column: span 2;
-    grid-row: span 2;
-}
-</style>";
       
       return [
         '#markup' => $output,
-        '#attached' => [
-          'library' => [
-              's3_gallery/https://cdn.jsdelivr.net/npm/nanogallery2@3.0.5/dist/jquery.nanogallery2.min.js',
-              's3_gallery/https://cdn.jsdelivr.net/npm/nanogallery2@3.0.5/dist/css/nanogallery2.min.css',
+        'css' => [
+          'theme' => [
+              'css/custom.css' => [],
           ],
         ],
         
