bb6ba9279c8dff0de6c808f30471fa84b15db518
Tim <47915253+tim-degroot@users.noreply.github.com>
2025-01-28 23:44:21 +0100

    use img to resize

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -266,9 +266,10 @@ private function photoPage($s3, $bucket, $prefix) {
   if (isset($contents['Contents'])) {
     foreach ($contents['Contents'] as $content) {
       $key = htmlspecialchars($content['Key']);
-      $key = str_replace('photos/', '', $key);
-      $url = "https://ik.imagekit.io/azqbjyakf/" . $key;
-      // $url = $s3->getObjectUrl($bucket, $key);
+      // $key = str_replace('photos/', '', $key);
+      // $url = "https://ik.imagekit.io/azqbjyakf/" . $key;
+      $url = $s3->getObjectUrl($bucket, $key);
+      // $url = "http://img.acdweb.nl/" . $url
       // $output .= "<li><img src=\"$url\" alt=\"$key\" style=\"max-width: 200px;\" /></li>";
       if (substr($url, -1) !== '/') {
         $output[] = $url;
